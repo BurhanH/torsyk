781109ca06a62f6a663cb547004821428fcb8c41
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-01-21 21:07:46 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Using bottom import as workaround to circular imports which is common problem with Flask applications
+# Using bottom import as a workaround to circular imports which is common problem with Flask applications
 from app import routes
