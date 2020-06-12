cb13cd6037bddf474d80209474d4e028bab38afe
Baur Urazalinov <BurhanH@users.noreply.github.com>
2020-06-12 17:53:48 -0500

    Update routes.py
    typo fix

--- app/routes.py
+++ app/routes.py
@@ -10,6 +10,6 @@ def index():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html', title='The page you were looking for doesn\'t exist (404)'), 404
+    return render_template('404.html', title='The page you were looking for does not exist (404)'), 404
 
 # TODO the same approach for 403, 410, 500
